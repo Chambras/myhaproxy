9c762a80c9ecabd0cc5899880acc995e3f8a7853
Marcelo Zambrana <marcelo.zambrana@dharbor.com>
2017-11-03 11:12:08 -0400

    add second node to lb

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,12 @@ node.default['haproxy']['members'] = [{
     'ipaddress' => '34.228.223.163',
     'port' => '80',
     'ssl_port' => '80'
-}]
+},{
+    'hostname' => 'ec2-34-203-233-173.compute-1.amazonaws.com',
+    'ipaddress' => '34.203.233.173',
+    'port' => '80',
+    'ssl_port' => '80'
+}
+]
 
 include_recipe 'haproxy::manual'
