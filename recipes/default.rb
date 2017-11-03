594205a042d73ceec9760ef1fb7b94753e014332
Marcelo Zambrana <marcelo.zambrana@dharbor.com>
2017-11-03 10:20:33 -0400

    using community cookbooks

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+    'hostname' => 'ec2-34-228-223-163.compute-1.amazonaws.com',
+    'ipaddress' => '34.228.223.163',
+    'port' => '80',
+    'ssl_port' => '80'
+}]
+
+include_recipe 'haproxy::manual'
